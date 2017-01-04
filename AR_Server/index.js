51e537861eed810f03bc993cc30b3fa8d7c30690
Unknown <Neffes1987@gmail.com>
2017-01-04 12:21:41 +0300

    fix

--- AR_Server/index.js
+++ AR_Server/index.js
@@ -1,14 +1,14 @@
 //index.js
 import MakeStore from './server/store'
 import startServer from './server/ioServer'
-import {db_init,db_insert,getRows} from './server/bd_core'
-let UserDB = db_init('DB');
-let LutDB = db_init('Lut');
-let CharDB = db_init('Char');
-let ActionDB = db_init('Action');
-let EventDB = db_init('Event');
-let PunishDB = db_init('Punish');
-let Model3D_DB = db_init('model3D');
+//import {db_init,db_insert,getRows} from './server/bd_core'
+//let UserDB = db_init('DB');
+//let LutDB = db_init('Lut');
+//let CharDB = db_init('Char');
+//let ActionDB = db_init('Action');
+//let EventDB = db_init('Event');
+//let PunishDB = db_init('Punish');
+//let Model3D_DB = db_init('model3D');
 
 //function eachRow(key,value){
 	//console.log(JSON.parse( value ))
@@ -20,8 +20,8 @@ let Model3D_DB = db_init('model3D');
 //getRows(db,'users',eachRow);
 
 
-//export const store = MakeStore();
-//store.dispatch({type:'SET_ENTRIES',	entries:require('./json/test.json')});
+export const store = MakeStore();
+store.dispatch({type:'SET_UP_STORE',store:require('./json/test.json')});
 //store.dispatch({type:'NEXT'});
 
 //startServer(store);
--- AR_Server/server/reducer.js
+++ AR_Server/server/reducer.js
@@ -3,11 +3,10 @@ import {setEntries,next,vote,initialState} from './core'
 
 export function reducer(state=initialState,action){
 	switch(action.type){
-		case 'SET_ENTRIES':return setEntries(state,action.entries);
+		case 'SET_UP_STORE':return setEntries(state,action.store);
 		case 'NEXT':return next(state);
 		case 'VOTE':return state.update('vote',state=>vote(state,action.entry));
 		default: return state;
 	}
 
 }
-
